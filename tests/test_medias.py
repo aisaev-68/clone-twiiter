5cde7f3634eac1f1333198c1389f16424521f76f
ramazan_aisaev <aisaev-68@yandex.ru>
2022-12-27 13:35:20 +0300

    fix_tests

--- tests/test_medias.py
+++ tests/test_medias.py
@@ -1,16 +1,14 @@
 import pytest
 from httpx import AsyncClient
-from routers.medias import router
 
 
 @pytest.mark.anyio
-async def test_add_tweets_likes():
-    async with AsyncClient(app=router, base_url="http://0.0.0.0") as ac:
-        with open("", mode="r", encoding="utf-8") as img_file:
-            data = img_file.read()
-        response = await ac.post("/api/medias", files=data, headers={"api-key": "test"})
-        assert response.status_code == 201
-        assert response.json() == {
-            "result": True,
-            "media_id": 1,
-        }
+async def test_add_tweets_likes(client: AsyncClient):
+    with open("", mode="r", encoding="utf-8") as img_file:
+        data = img_file.read()
+    response = await client.post("/api/medias", files=data, headers={"api-key": "test"})
+    assert response.status_code == 201
+    assert response.json() == {
+        "result": True,
+        "media_id": 1,
+    }
